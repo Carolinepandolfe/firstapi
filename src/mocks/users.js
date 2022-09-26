ca1faa3548382e6ae103d85d81c5a6f70d9b7314
carolpandolfe <carolinepandolfe@yahoo.com.br>
2022-09-26 19:27:01 -0300

    Initial commit

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Carol",
+  },
+  {
+    id: 2,
+    name: "Caroline",
+  },
+  {
+    id: 3,
+    name: "Soares",
+  },
+  {
+    id: 4,
+    name: "Pandolfe",
+  },
+];
